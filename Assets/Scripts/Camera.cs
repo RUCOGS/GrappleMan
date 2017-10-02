4121be0dd1d9751f5e7c99b57e8d87e5cbdcd672
Aaron Kau <aaron.kau@rutgers.edu>
2017-10-01 23:34:24 -0400

    Add Camera Script to Follow Player.

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour
+{
+
+    public Player player;
+
+    //Offset in values between camera and player. 
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start()
+	{
+        //Assume Camera and Player start in the desired position alignment.
+        //The current offset between Camera and Player will be maintained.
+        offset = transform.position - player.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+        //Every frame, move camera's position to player's position (Also accounting for offset).
+        transform.position = player.transform.position + offset;
+	}
+}
