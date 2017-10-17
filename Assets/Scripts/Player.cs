5e385cf7cf1d02c76fb6542f3f98284894fbd8c9
Gabriel <gabeajh@gmail.com>
2017-10-17 13:37:46 -0400

    Auto stash before merge of "master" and "origin/master"

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,17 +13,36 @@ public class Player : MonoBehaviour {
     public float groundFriction;
     public float wallJumpVelocity;
     public float wallJumpAngleFromVertical;
+    public float ropeDistance;
+    public float ropeSpeed;
+    public LayerMask collisionMask;
 
     ControllableBox physics;
 
     bool doubleJump = false;
     bool pressJumpLast = false;
+    bool grapple = false;
 
     Vector2 startPosition;
+    Vector2 ropeDirection;
 
     void Start() {
         physics = GetComponent<ControllableBox>();
         startPosition = new Vector2(transform.position.x, transform.position.y);
+    }
+
+    void Update() {
+        if(!grapple) {
+            if (Input.GetMouseButtonDown(0)) {
+                Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector2 playerPosition = transform.position;
+                RaycastHit2D raycastHit = Physics2D.Raycast(playerPosition, mousePosition - playerPosition, ropeDistance, collisionMask);
+                if (raycastHit) {
+                    ropeDirection = raycastHit.point - playerPosition;
+                    grapple = true;
+                }
+            }
+        }
     }
 
     void FixedUpdate() {
@@ -64,6 +83,12 @@ public class Player : MonoBehaviour {
                 physics.Accel(new Vector2(0, doubleJumpVelocity));
                 doubleJump = false;
             }
+        }
+
+        if (grapple) {
+            grapple = false;
+            ropeDirection.Normalize();
+            physics.Accel(ropeDirection * ropeSpeed);
         }
 
         pressJumpLast = attemptJump;
