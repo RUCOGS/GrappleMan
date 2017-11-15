8a16f3ddbacaa6ae881b37c226a1789800124824
Gabriel <gabeajh@gmail.com>
2017-11-14 20:19:03 -0500

    Level0 and Level1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,27 +6,23 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     public float gravity;
-    public float jumpVelocity;
-    public float doubleJumpVelocity;
-    public float groundSpeed;
-    public float airSpeed;
     public float groundFriction;
-    public float wallJumpVelocity;
-    public float wallJumpAngleFromVertical;
-    public float ropeDistance;
+    //public float ropeDistance;
     public float ropePullSpeed;
-    public LayerMask collisionMask;
+    //public float ropeSwingSpeed;
+    public LayerMask ropeCollisionMask;
 
     ControllableBox physics;
     LineRenderer rope;
-
-    bool doubleJump = false;
-    bool pressJumpLast = false;
+    
     bool ropePull = false;
     bool ropeGrapple = false;
 
     Vector2 startPosition;
     Vector2 ropeDirection;
+
+    float ropeRadius = 0;
+    float ropeAngle = 0;
 
     void Start() {
         physics = GetComponent<ControllableBox>();
@@ -39,36 +35,37 @@ public class Player : MonoBehaviour {
             if (Input.GetMouseButtonDown(0)) {
                 Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 Vector2 playerPosition = transform.position;
-                RaycastHit2D raycastHit = Physics2D.Raycast(playerPosition, mousePosition - playerPosition, ropeDistance, collisionMask);
+                RaycastHit2D raycastHit = Physics2D.Raycast(playerPosition, mousePosition - playerPosition, Mathf.Infinity, ropeCollisionMask);
                 if (raycastHit) {
                     ropeDirection = raycastHit.point - playerPosition;
                     rope.SetPosition(0, Vector2.zero);
-                    rope.SetPosition(1, raycastHit.point - playerPosition);
+                    float width = transform.GetComponent<Collider2D>().bounds.size.x;
+                    float height = transform.GetComponent<Collider2D>().bounds.size.y;
+                    rope.SetPosition(1, ropeDirection + new Vector2(width/2, height/2));
                     rope.enabled = true;
                     ropePull = true;
                 }
             }
         }
 
-        if(!ropeGrapple) {
-            if(Input.GetMouseButtonDown(1)) {
-                Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                Vector2 playerPosition = transform.position;
-                RaycastHit2D raycastHit = Physics2D.Raycast(playerPosition, mousePosition - playerPosition, ropeDistance, collisionMask);
-                if (raycastHit) {
-                    ropeDirection = raycastHit.point - playerPosition;
-                    rope.SetPosition(0, Vector2.zero);
-                    rope.SetPosition(1, raycastHit.point - playerPosition);
-                    rope.enabled = true;
-                    ropeGrapple = true;
-                }
-            }
-        }
+        //if(!ropeGrapple) {
+        //    if(Input.GetMouseButtonDown(1)) {
+        //        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //        Vector2 playerPosition = transform.position;
+        //        RaycastHit2D raycastHit = Physics2D.Raycast(playerPosition, mousePosition - playerPosition, ropeDistance, ropeCollisionMask);
+        //        if (raycastHit) {
+        //            ropeDirection = raycastHit.point - playerPosition;
+        //            rope.SetPosition(0, Vector2.zero);
+        //            rope.SetPosition(1, ropeDirection);
+        //            rope.enabled = true;
+        //            ropeGrapple = true;
+        //        }
+        //    }
+        //}
     }
 
     void FixedUpdate() {
         float sideMovement = Input.GetAxisRaw("Horizontal");
-        bool attemptJump = Input.GetKey(KeyCode.Space);
 
         Vector2 vel = physics.GetVelocity();
 
@@ -86,43 +83,45 @@ public class Player : MonoBehaviour {
             }
         }
 
-        if (!ropeGrapple) {
-            physics.Accel(new Vector2(0, gravity));
-            if (physics.IsOnGround()) {
-                physics.Accel(groundSpeed * sideMovement, 0);
-                doubleJump = true;
-                if (attemptJump) {
-                    physics.Accel(new Vector2(0, jumpVelocity));
-                }
-                physics.Accel(new Vector2(vel.x * groundFriction, 0));
-            }
-            else if (physics.IsOnLeftWall() && attemptJump && !pressJumpLast) {
-                physics.Accel(new Vector2(wallJumpVelocity * Mathf.Sin(Mathf.PI * wallJumpAngleFromVertical / 180), wallJumpVelocity * Mathf.Cos(Mathf.PI * wallJumpAngleFromVertical / 180)));
-            }
-            else if (physics.IsOnRightWall() && attemptJump && !pressJumpLast) {
-                physics.Accel(new Vector2(-1 * wallJumpVelocity * Mathf.Sin(Mathf.PI * wallJumpAngleFromVertical / 180), wallJumpVelocity * Mathf.Cos(Mathf.PI * wallJumpAngleFromVertical / 180)));
-            }
-            else if (!ropeGrapple) {
-                physics.Accel(airSpeed * sideMovement, 0);
-                if (doubleJump && attemptJump && !pressJumpLast) {
-                    physics.Accel(new Vector2(0, doubleJumpVelocity));
-                    doubleJump = false;
-                }
-            }
-
-            if (ropePull) {
-                ropeDirection.Normalize();
-                physics.Accel(ropeDirection * ropePullSpeed);
-                ropePull = false;
-                rope.enabled = false;
-            }
-        } else if(ropeGrapple) {
-            ropeDirection.Normalize();
-            physics.Accel(-physics.GetVelocity());
-            //physics.Accel(ropeDirection * sideMovement);
+        if(!ropeGrapple) {
+            physics.Accel(0, gravity);
         }
 
-        pressJumpLast = attemptJump;
+        if(physics.IsOnGround()) {
+            //physics.Accel(-vel.x, -vel.y);
+            physics.Accel(vel.x * groundFriction, 0);
+        }
+
+        if (rope.enabled) {
+            rope.SetPosition(0, Vector2.zero);
+            float width = transform.GetComponent<Collider2D>().bounds.size.x;
+            float height = transform.GetComponent<Collider2D>().bounds.size.y;
+            rope.SetPosition(1, ropeDirection + new Vector2(width / 2, height / 2));
+            if (vel.y <= 0 && !ropeGrapple) {
+                rope.enabled = false;
+            }
+        }
+
+        if (ropePull) {
+            ropeDirection.Normalize();
+            if(vel.y < 0) {
+                physics.Accel(ropeDirection * (ropePullSpeed-gravity));
+            } else {
+                physics.Accel(ropeDirection * ropePullSpeed);
+            }
+            ropePull = false;
+        }
+
+        //if(ropeGrapple) {
+        //    ropeDirection.Normalize();
+        //    physics.Accel(-physics.GetVelocity());
+        //    if(sideMovement != 0) {
+        //        ropeRadius = (rope.GetPosition(1) - rope.GetPosition(0)).magnitude;
+        //        ropeAngle += ropeSwingSpeed * sideMovement;
+        //        Vector2 swingPlayer = new Vector2(Mathf.Sin(ropeAngle), Mathf.Cos(ropeAngle)) * ropeRadius;
+        //        physics.Accel(swingPlayer);
+        //    }
+        //}
 
         physics.runPhysics();
     }
